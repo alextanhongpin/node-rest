6fed6f7be028734c96683b6e44b3d5065f92a8e8
Alex Tan Hong Pin <alextan@seekasia.com>
2018-04-25 17:11:03 +0800

    refactor: change logic for database

--- src/database/index.js
+++ src/database/index.js
@@ -10,7 +10,7 @@
 
 import mysql from 'mysql2/promise'
 
-async function connect ({ pool, host, user, database, password, port }) {
+async function Database ({ pool, host, user, database, password, port }) {
   // Use connection pool to avoid sudden termination of the connection
   const connection = mysql.createPool({
     connectionLimit: pool,
@@ -24,4 +24,4 @@ async function connect ({ pool, host, user, database, password, port }) {
   return connection
 }
 
-export default { connect }
+export default Database
